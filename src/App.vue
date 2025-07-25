663e1139377f28cb1454e94df1f89c4b65cccfc6
langxuewenya <langxuewenya@163.com>
2025-07-26 02:25:57 +0800

    style(element-plus style): update element-plus style
    element-plus使用中文

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="app">
-    <router-view />
+    <el-config-provider :locale="zhCn">
+      <router-view />
+    </el-config-provider>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+    return {
+      zhCn
+    };
+  }
+});
+</script>
 
 <style lang="less"></style>
